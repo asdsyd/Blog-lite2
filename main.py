5855e012f717e73aa11faf4eda5800ac33d8955f
Asad Sayeed <ashasd97@gmail.com>
2023-01-01 23:20:17 +0530

    db models

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '11cbef399d872e8001d3aa2a92a34c16'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), unique=True, nullable=False)
 
 posts = [
     {
